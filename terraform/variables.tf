75f703541e30396e100c00950e30487a3721f755
hngiap94 <hngiap94@gmail.com>
2023-11-20 00:39:11 +0700

    update workflow

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,34 +15,34 @@ variable public_subnets {
   type = map
 }
 
-# variable docker_image_tag {
-#   type = string
-# }
+variable docker_image_tag {
+  type = string
+}
 
-# variable postgres_user {
-#   type = string
-# }
+variable postgres_user {
+  type = string
+}
 
-# variable postgres_password {
-#   type = string
-# }
+variable postgres_password {
+  type = string
+}
 
-# variable github_token {
-#   type = string
-# }
+variable github_token {
+  type = string
+}
 
-# variable github_client_id {
-#   type = string
-# }
+variable github_client_id {
+  type = string
+}
 
-# variable github_client_secret {
-#   type = string
-# }
+variable github_client_secret {
+  type = string
+}
 
-# variable access_key_id {
-#   type = string
-# }
+variable access_key_id {
+  type = string
+}
 
-# variable secret_access_key {
-#   type = string
-# }
+variable secret_access_key {
+  type = string
+}
